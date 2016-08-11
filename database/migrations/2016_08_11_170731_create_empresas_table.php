4c1da21ec7e337be68b014c578d0a69ea27f5af0
Héctor Villa <dnsdirmx@outlook.com>
2016-08-11 12:08:55 -0500

    creado el modelo de empresa

---
+++ database/migrations/2016_08_11_170731_create_empresas_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmpresasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empresas', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('nombre');
+            $table->string('ciudad');
+            $table->string('direccion');
+            $table->string('telefono');
+
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
+
+            $table->integer('giro_id')->unsigned();
+            $table->string('cod_promotor');
+
+            $table->timestamps();
+
+            $table->foreign('giro_id')->references('id')->on('giros');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('empresas');
+    }
+}
